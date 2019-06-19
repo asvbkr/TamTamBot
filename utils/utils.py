9eb75a476bde1623ef622b665897c9f71d5d2020
ASV <asv@bk.ru>
2019-06-19 22:25:08 +0300

    минорные правки

--- utils/utils.py
+++ utils/utils.py
@@ -35,6 +35,8 @@ def calling_function_name(level=2):
 def get_param_value(content, parameter, ends='{}'):
     # type: (str, str, str) -> str
     # Получение параметра из строки вида: '{{cmd=/street}}'
+    content = content or ''
+    parameter = parameter or ''
     found = re.match('.*(%s%s=(.+?)%s).*' % ('\\' + ends[0], parameter, '\\' + ends[1]), content)
     if found:
         return found.group(2)
